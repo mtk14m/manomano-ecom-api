f49f8521cf6dfd0d6a07f48f85744d365cb2c725
Minou Matoko <mtk14minou@gmail.com>
2026-04-06 20:15:16 +0200

    [some] refactor des handlers ajout de helpers

--- internal/handlers/product.go
+++ internal/handlers/product.go
@@ -37,27 +37,14 @@ func (h *ProductHandler) GetProducts(c *gin.Context) {
 }
 
 func (h *ProductHandler) GetProductByID(c *gin.Context) {
-	id, err := strconv.Atoi(c.Param("id"))
+	id, err := parseProductID(c)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "invalid product id",
-		})
 		return
 	}
 
 	product, err := h.repo.GetByID(id)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": "product not found",
-			})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "internal server error",
-		})
+	if handleProductRepoError(c, err) {
 		return
-
 	}
 
 	c.JSON(http.StatusOK, product)
@@ -66,43 +53,7 @@ func (h *ProductHandler) GetProductByID(c *gin.Context) {
 func (h *ProductHandler) CreateProduct(c *gin.Context) {
 	var dto dtos.CreateProductDto
 
-	if err := c.ShouldBindJSON(&dto); err != nil {
-		var validationErrs validator.ValidationErrors
-
-		if errors.As(err, &validationErrs) {
-			out := make(map[string]string)
-
-			for _, fieldErr := range validationErrs {
-				field := strings.ToLower(fieldErr.Field())
-
-				switch fieldErr.Tag() {
-				case "required":
-					out[field] = "this field is required"
-				case "min":
-					out[field] = fmt.Sprintf("must be at least %s", fieldErr.Param())
-				case "max":
-					out[field] = fmt.Sprintf("must be at most %s", fieldErr.Param())
-				case "gte":
-					out[field] = fmt.Sprintf("must be greater than or equal to %s", fieldErr.Param())
-				case "gt":
-					out[field] = fmt.Sprintf("must be greater than %s", fieldErr.Param())
-				case "lte":
-					out[field] = fmt.Sprintf("must be less than or equal to %s", fieldErr.Param())
-				default:
-					out[field] = fmt.Sprintf("failed on '%s' validation", fieldErr.Tag())
-				}
-			}
-
-			c.JSON(http.StatusBadRequest, gin.H{
-				"error":   "validation failed",
-				"details": out,
-			})
-			return
-		}
-
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "invalid request payload",
-		})
+	if !bindAndValidateJSON(c, &dto) {
 		return
 	}
 
@@ -125,16 +76,48 @@ func (h *ProductHandler) CreateProduct(c *gin.Context) {
 }
 
 func (h *ProductHandler) UpdateProduct(c *gin.Context) {
-	id, err := strconv.Atoi(c.Param("id"))
+
+	var dto dtos.UpdateProductDto
+	id, err := parseProductID(c)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "invalid product id",
-		})
 		return
 	}
-	var dto dtos.UpdateProductDto
 
-	if err := c.ShouldBindJSON(&dto); err != nil {
+	if !bindAndValidateJSON(c, &dto) {
+		return
+	}
+	p := models.Product{
+		Name:     dto.Name,
+		Price:    dto.Price,
+		Category: dto.Category,
+		InStock:  dto.InStock,
+	}
+
+	product, err := h.repo.Update(id, p)
+	if handleProductRepoError(c, err) {
+		return
+	}
+	c.JSON(http.StatusOK, product)
+}
+
+func (h *ProductHandler) DeleteProduct(c *gin.Context) {
+	id, err := parseProductID(c) // on valide le param
+	if err != nil {
+		return
+	}
+	err = h.repo.Delete(id)
+	if handleProductRepoError(c, err) {
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
+
+//HELPERS
+
+func bindAndValidateJSON(c *gin.Context, dto any) bool {
+
+	if err := c.ShouldBindJSON(dto); err != nil {
 		var validationErrs validator.ValidationErrors
 
 		if errors.As(err, &validationErrs) {
@@ -165,63 +148,43 @@ func (h *ProductHandler) UpdateProduct(c *gin.Context) {
 				"error":   "validation failed",
 				"details": out,
 			})
-			return
+			return false
 		}
 
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "invalid request payload",
 		})
-		return
+		return false
 	}
 
-	p := models.Product{
-		Name:     dto.Name,
-		Price:    dto.Price,
-		Category: dto.Category,
-		InStock:  dto.InStock,
-	}
-
-	product, err := h.repo.Update(id, p)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": "product not found",
-			})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "internal server error",
-		})
-		return
-	}
-	c.JSON(http.StatusOK, product)
+	return true
 }
 
-func (h *ProductHandler) DeleteProduct(c *gin.Context) {
-	idParam := c.Param("id")
-
-	id, err := strconv.Atoi(idParam)
+func parseProductID(c *gin.Context) (int, error) {
+	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "invalid product id",
 		})
-		return
+		return -1, err
+	}
+	return id, nil
+}
+
+func handleProductRepoError(c *gin.Context, err error) bool {
+	if err == nil {
+		return false
 	}
 
-	err = h.repo.Delete(id)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": "product not found",
-			})
-			return
-		}
-
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "internal server error",
+	if errors.Is(err, sql.ErrNoRows) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "product not found",
 		})
-		return
+		return true
 	}
 
-	c.Status(http.StatusNoContent)
+	c.JSON(http.StatusInternalServerError, gin.H{
+		"error": "internal server error",
+	})
+	return true
 }
